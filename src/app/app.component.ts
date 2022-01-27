c3ac64999e8ac39ddff3bd28f49c2ac482e2da85
Marlon <mrosalio502@gmail.com>
2022-01-27 11:04:20 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store';
+  title = 'Pepito';
+
+  getName(): void {
+    console.log('Hola Pepito');
+  }
 }
